4bff3177763b1b7f5c1fdcfdac122f2c041be94c
Kazunori Akiyama <kakiyama@mit.edu>
2025-01-04 16:51:23 -0500

    import fftfreqs

--- src/AbstractFHTs.jl
+++ src/AbstractFHTs.jl
@@ -1,5 +1,5 @@
 module AbstractFHTs
-import AbstractFFTs: Plan, eltype, size, fftdims
+import AbstractFFTs: Plan, eltype, size, fftdims, fftfreqs
 import Base: size, ndims, length, eltype,
              *, inv, \, size
 import LinearAlgebra
